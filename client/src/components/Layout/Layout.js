9a218382dc4300a91588277ce73724ddd93ea816
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-01-29 22:05:45 +0530

    React SEO functionality using Helmet

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
+import {Helmet} from 'react-helmet';
 
-const Layout = (props) => {
+
+const Layout = ({children,title, description,keywords,author}) => {
   return (
   <div>
+    <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+    </Helmet>
     <Header/>
     <main style={{minHeight:"70vh"}}>
-    {props.children}
+    {children}
     </main>
     <Footer/>
   </div>
   );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Ashutosh SIngh",
 };
 
 export default Layout;
