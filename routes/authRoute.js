67719450b15222ad24ee3fe74aa43588509e5158
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-02-03 14:23:35 +0530

    Admin dashboard and user dashboard functionality

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -22,8 +22,13 @@ router.post ('/forgot-password',forgotPasswordController)
 
 router.get('/test',requireSignIn,isAdmin,testController);
 
-// protected routes
+// protected User routes auth
 router.get('/user-auth',requireSignIn,(req,res)=>{
+    res.status(200).send({ok:true});
+})
+
+// protected admin Route auth
+router.get('/admin-auth',requireSignIn,isAdmin,(req,res)=>{
     res.status(200).send({ok:true});
 })
 
